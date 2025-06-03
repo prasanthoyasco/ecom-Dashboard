0ecb1ea582a1ac4547ddc8de62411e8fcd67134b
prasanthoyasco <prasanthoyasco@gmai.com>
2025-06-03 18:30:45 +0530

    error resolve

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Sidebar from "../components/organisms/sidebar";
+import Sidebar from "../components/organisms/Sidebar";
 import Header from '../components/organisms/Header';
 
 export default function DashboardLayout() {
